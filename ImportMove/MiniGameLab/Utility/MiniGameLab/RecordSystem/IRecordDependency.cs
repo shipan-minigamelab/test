bd1bc784ed16ceecb38f6052f61b5b8a5f11776c
Shipan Mazumder <shipanmazumder@gmail.com>
2022-06-06 18:38:02 +0600

    delete

--- ImportMove/MiniGameLab/Utility/MiniGameLab/RecordSystem/IRecordDependency.cs
+++
@@ -1,9 +0,0 @@
-public interface IRecordDependency
-{
-    public int GetCurrentAnimation => GetCurrentAnimIndex();
-    public bool PauseRecorder => GetPauseValue();
-    public int GetCurrentAnimIndex();
-    public void PlayAnimation(int index);
-
-    public bool GetPauseValue();
-}
